90bfa038f32189e2b65b061e0a5864b28bffa210
SeungHyunKim85 <kshrtg85@gmail.com>
2019-10-08 15:45:53 +0900

    using Context API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import TodoTemplete from './Components/TodoTemplete';
 import TodoHead from './Components/TodoHead';
 import TodoList from './Components/TodoList';
